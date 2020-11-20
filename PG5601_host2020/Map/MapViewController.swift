c8176744fcb229f6b417a9863d9b2643079308f9
mayamariakj <maya_t_kjaer@hotmail.com>
2020-11-20 20:35:09 +0100

    added pin to map

--- PG5601_host2020/Map/MapViewController.swift
+++ PG5601_host2020/Map/MapViewController.swift
@@ -4,6 +4,8 @@
 //
 //  Created by Maya maria Kjær on 02/11/2020.
 //  Copyright © 2020 Maya maria Kjær. All rights reserved.
+//https://stackoverflow.com/questions/47987473/addressdictionary-is-deprecated-first-deprecated-in-ios-11-0-use-properties
+//https://stackoverflow.com/questions/34431459/ios-swift-how-to-add-pinpoint-to-map-on-touch-and-get-detailed-address-of-th
 // https://medium.com/@kiransjadhav111/corelocation-map-kit-get-the-users-current-location-set-a-pin-in-swift-edb12f9166b2
 
 import Foundation
@@ -11,23 +13,37 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate{
+class MapViewController: UIViewController, CLLocationManagerDelegate{
     
     var locationManager: CLLocationManager!
     var currentLocationStr = "Current location"
+    var currentLocation: CLLocationCoordinate2D!
+    var pinLocation: CLLocationCoordinate2D!
     
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var mapInformationView: MapInformationView!
-    
+    @IBOutlet weak var userPinSwitch: UISwitch!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        mapView.delegate = self
+        let mapTapGesture = UITapGestureRecognizer(target: self, action: #selector(mapTap))
+        mapView.addGestureRecognizer(mapTapGesture)
+        userPinSwitch.addTarget(self, action: #selector(pinSwitchChanged), for:UIControl.Event.valueChanged)
     }
     
     override func viewDidAppear(_ animated: Bool) {
         determineCurrentLocation()
     }
+    
+    @objc func pinSwitchChanged(pinSwitch: UISwitch){
+        if pinSwitch.isOn {
+            centerUserOnLocationAndAddPin(location: currentLocation)
+        }else{
+            centerUserOnLocationAndAddPin(location: pinLocation)
+        }
+    }
+    
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print("Error - locationManager: \(error.localizedDescription)")
@@ -47,41 +63,97 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let mUserLocation:CLLocation = locations[0] as CLLocation
         let center = CLLocationCoordinate2D(latitude: mUserLocation.coordinate.latitude, longitude: mUserLocation.coordinate.longitude)
-        let mRegion = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
-        mapView.setRegion(mRegion, animated: true)
         
+        centerUserOnLocationAndAddPin(location: center)
+        currentLocation = center
         //set The Coordinats For weather screen.
         setWeatherCoordinates(latitude: mUserLocation.coordinate.latitude, longitude: mUserLocation.coordinate.longitude)
         
         self.mapInformationView.latitudeData.text = String(mUserLocation.coordinate.latitude)
         self.mapInformationView.longitudeData.text = String(mUserLocation.coordinate.longitude)
         self.mapInformationView.weatherImage.image = UIImage(named: "clearsky_day")
-
+        
         // Get user's Current Location and Drop a pin
-        let mkAnnotation: MKPointAnnotation = MKPointAnnotation()
-            mkAnnotation.coordinate = CLLocationCoordinate2DMake(mUserLocation.coordinate.latitude, mUserLocation.coordinate.longitude)
-            mkAnnotation.title = self.setUsersClosestLocation(mLattitude: mUserLocation.coordinate.latitude, mLongitude: mUserLocation.coordinate.longitude)
-            mapView.addAnnotation(mkAnnotation)
+       
     }
-    //MARK:- Intance Methods
+    
+    func centerUserOnLocationAndAddPin(location: CLLocationCoordinate2D){
+        let mRegion = MKCoordinateRegion(center: location, span: MKCoordinateSpan(latitudeDelta: 0.07, longitudeDelta: 0.07))
+        self.mapView.setRegion(mRegion, animated: true)
+        let mkAnnotation: MKPointAnnotation = MKPointAnnotation()
+        mkAnnotation.coordinate = CLLocationCoordinate2DMake(location.latitude, location.longitude)
+        mkAnnotation.title = self.setUsersClosestLocation(mLattitude: location.latitude, mLongitude: location.longitude)
+        self.mapView.removeAnnotations(self.mapView.annotations)
+        self.mapView.addAnnotation(mkAnnotation)
+    }
 
     func setUsersClosestLocation(mLattitude: CLLocationDegrees, mLongitude: CLLocationDegrees) -> String {
-        let geoCoder = CLGeocoder()
         let location = CLLocation(latitude: mLattitude, longitude: mLongitude)
+        
+       let geocoder = CLGeocoder()
+           geocoder.reverseGeocodeLocation(location) { (placemarksArray, error) in
+               print(placemarksArray!)
+               if (error) == nil {
+                   if placemarksArray!.count > 0 {
+                       let placemark = placemarksArray?[0]
+                       let address = "\(placemark?.subThoroughfare ?? ""), \(placemark?.thoroughfare ?? ""), \(placemark?.locality ?? ""), \(placemark?.subLocality ?? ""), \(placemark?.administrativeArea ?? ""), \(placemark?.postalCode ?? ""), \(placemark?.country ?? "")"
+                       print("\(address)")
+                   }
+               }
 
-        geoCoder.reverseGeocodeLocation(location) {
-            (placemarks, error) -> Void in
-
-            if let mPlacemark = placemarks{
-                if let dict = mPlacemark[0].addressDictionary as? [String: Any]{
-                    if let Name = dict["Name"] as? String{
-                        if let City = dict["City"] as? String{
-                            self.currentLocationStr = Name + ", " + City
-                        }
-                    }
-                }
-            }
-        }
+           }
         return currentLocationStr
     }
+    
+    @objc func mapTap(sender: UIGestureRecognizer){
+        print("maptap")
+        if sender.state == .ended {
+            let locationInView = sender.location(in: mapView)
+            let locationOnMap = mapView.convert(locationInView, toCoordinateFrom: mapView)
+            addAnnotation(location: locationOnMap)
+            print("setting location")
+            pinLocation = locationOnMap
+        }
+    }
+
+    func addAnnotation(location: CLLocationCoordinate2D){
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        annotation.title = self.setUsersClosestLocation(mLattitude: location.latitude, mLongitude: location.longitude)
+        self.mapView.removeAnnotations(self.mapView.annotations)
+        self.mapView.addAnnotation(annotation)
+    }
+}
+
+extension MapViewController: MKMapViewDelegate{
+
+func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+    guard annotation is MKPointAnnotation else { print("no mkpointannotaions"); return nil }
+
+    let reuseId = "pin"
+    var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+
+    if pinView == nil {
+        pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+        pinView!.canShowCallout = true
+        pinView!.rightCalloutAccessoryView = UIButton(type: .infoDark)
+        pinView!.pinTintColor = UIColor.black
+    }
+    else {
+        pinView!.annotation = annotation
+    }
+    return pinView
+}
+
+func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+    print("tapped on pin ")
+}
+
+func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+    if control == view.rightCalloutAccessoryView {
+        if let doSomething = view.annotation?.title! {
+           print("do something")
+        }
+    }
+  }
 }
